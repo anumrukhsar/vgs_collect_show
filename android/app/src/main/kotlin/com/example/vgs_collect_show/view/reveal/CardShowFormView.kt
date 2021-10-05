d92ab09ebdecf2cbba55dd82ccd5c62b47769ed2
Anum Rukhsar <anum.rukhsar072@gmail.com>
2021-10-05 16:46:28 +0500

    seperate reveal page

--- android/app/src/main/kotlin/com/example/vgs_collect_show/view/reveal/CardShowFormView.kt
+++ android/app/src/main/kotlin/com/example/vgs_collect_show/view/reveal/CardShowFormView.kt
@@ -6,6 +6,9 @@ import com.verygoodsecurity.vgsshow.core.network.client.VGSHttpMethod
 import com.example.vgs_collect_show.MainActivity
 import com.example.vgs_collect_show.R
 import com.example.vgs_collect_show.view.BaseFormView
+import com.verygoodsecurity.vgsshow.core.listener.VGSOnResponseListener
+import com.verygoodsecurity.vgsshow.core.network.model.VGSRequest
+import com.verygoodsecurity.vgsshow.core.network.model.VGSResponse
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -44,4 +47,31 @@ class CardShowFormView constructor(context: Context, messenger: BinaryMessenger?
             ))
         }
     }
+//private fun revealCard(call: MethodCall) {
+//    val data = call.arguments as ArrayList<*>
+//    val headerName = "Authorization";
+//    val headerval = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJCaVRrRjlMbTA4d2hWM2x1QXFfQks1WHpTbVFoVVhGOTBJV3o3XzJWS1dBIn0.eyJleHAiOjE2MzI3MjI2NDYsImlhdCI6MTYzMjcyMjM0NiwianRpIjoiZTFiYjk2MjctMGVlNS00NTQyLWIwNDUtMTIzN2ZlMzM4YWIyIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLWRldi5jb250YXN3YXAuaW8vYXV0aC9yZWFsbXMvc29raW4iLCJhdWQiOlsicmVhbG0tbWFuYWdlbWVudCIsImFjY291bnQiXSwic3ViIjoiZTRjZmUzZDEtYWNhNC00ODc4LWEzZmEtYzNiYzI5OWQ1MzUyIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic29raW4iLCJzZXNzaW9uX3N0YXRlIjoiMWU5OTE0MjMtMzUzNi00NjRlLTkwZjQtOTM2NmFkYjk1OTg2IiwiYWNyIjoiMSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsicmVhbG0tbWFuYWdlbWVudCI6eyJyb2xlcyI6WyJtYW5hZ2UtdXNlcnMiXX0sInNva2luIjp7InJvbGVzIjpbInVtYV9wcm90ZWN0aW9uIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6IiIsImluc3RpdHV0aW9uX3JlZiI6IjcyNjczQjlBOUIwOUI1NTZBNDlFOTRGRDY0Rjk2ODBCIiwiY2xpZW50SWQiOiJzb2tpbiIsImNsaWVudEhvc3QiOiI1Mi4yLjE4My44NSIsImNsaWVudEFkZHJlc3MiOiI1Mi4yLjE4My44NSJ9.h7dOS6I2K7P3UMnT8kKW3wNEMH4fw9IjjAFqSfdXr7gcrRR55yTgvY5mBf9PFpF5afy5tp5VGOXU7MSAVf6D8sfLS3Byy1qxYRBbCPjbRiNLvCHC_1Eg1dnKkUYq580eRYWhejepkJhf04vdzwsPSIbRqltzheYG7jshauOda4LKHGHkYiX-6gBFUaYIJ7FEh0EpZVC30AmUuCzBXr8cmK2stCKdB8NlN4ZSVIQuX_fV-0Dkfe7zNigPDY5u8gYW-htVFUaTH8g1RsEYBzdLb-AB5X0AfNrFgFPx-9CltRTxJfOdZXtyfcfFHZC0L2-FOR5q6OHHJ1Pk7bSwdCIW7w";
+//    runOnBackground {
+//        val request = VGSRequest.Builder("/beta_v3/virtual_cards/1", VGSHttpMethod.GET)
+//            .headers(mapOf(headerName to headerval))
+////            .setHostname("swapcards.com.br")
+//            .build()
+//
+//        vgsShow.requestAsync(request)
+//        vgsShow.addOnResponseListener(object :VGSOnResponseListener{
+//            override fun onResponse(response: VGSResponse) {
+//                when (response) {
+//                    is VGSResponse.Success -> {
+//                        val successCode = response.code
+//                    }
+//                    is VGSResponse.Error -> {
+//                        val errorCode = response.code
+//                        val message = response.message
+//                    }
+//                }            }
+//        }
+//        )
+//    }
+//}
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,19 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class ShowHomePage extends StatefulWidget {
+  ShowHomePage({Key key, this.title,this.cardToken,this.dateToken,this.nameToken,this.cvcToken,this.ssnToken}) : super(key: key);
+  final String title;
+  final String cardToken;
+  final String dateToken;
+  final String nameToken;
+  final String cvcToken;
+  final String ssnToken;
+
+  @override
+  _ShowHomePageState createState() => _ShowHomePageState();
+}
+
 const COLLECT_FORM_VIEW_TYPE = 'card-collect-form-view';
 const SHOW_FORM_VIEW_TYPE = 'card-show-form-view';
 
@@ -53,7 +66,6 @@ const SHOW_ERROR_MESSAGE_KEY = 'show_error_message';
 
 class _MyHomePageState extends State<MyHomePage> {
   CardCollectFormController collectController;
-  CardShowFormController showController;
 
   String cardToken;
   String dateToken;
@@ -99,30 +111,13 @@ class _MyHomePageState extends State<MyHomePage> {
                           padding: const EdgeInsets.only(bottom: 12.0),
                           child: _collectButton(),
                         ),
-                  ]),
-                ),
-                ConstrainedBox(
-                    constraints: BoxConstraints(maxHeight: 400),
-                    child: Column(children: <Widget>[
-                      Text(
-                        'Show Card Details',
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.grey.shade700),
-                      ),
-                      Flexible(child: Builder(builder: (context) {
-                        // any logic needed...
-                        final isAndorid =
-                            defaultTargetPlatform == TargetPlatform.android;
-
-                        return isAndorid ? _cardShow() : _cardShowNativeiOS();
-                      })),
                       Padding(
                         padding: const EdgeInsets.only(bottom: 12.0),
-                        child: _showButton(),
+                        child: _revealButton(),
                       ),
-                    ])),
+                  ]),
+                ),
+
               ],
             ),
           ),
@@ -211,41 +206,25 @@ class _MyHomePageState extends State<MyHomePage> {
       },
     );
   }
+  Widget _revealButton() {
+    return MaterialButton(
+      minWidth: MediaQuery.of(context).size.width,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      color: Colors.blue,
+      child: new Text('Go to Reveal Page',
+          style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+      onPressed: () {
+       Navigator.push(context, new MaterialPageRoute(builder: (context)=>ShowHomePage(title: 'Reveal Page',cardToken: cardToken,dateToken:dateToken,nameToken: nameToken,cvcToken: cvcToken,ssnToken: ssnToken,)));
+      },
+    );
+  }
 
   void _createCardCollectController(int id) {
     print("View id = $id");
     collectController = new CardCollectFormController._(id);
   }
 
-  Widget _cardShow() {
-    final Map<String, dynamic> creationParams = <String, dynamic>{};
-    return PlatformViewLink(
-      viewType: SHOW_FORM_VIEW_TYPE,
-      surfaceFactory:
-          (BuildContext context, PlatformViewController controller) {
-        return AndroidViewSurface(
-          controller: controller,
-          gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
-          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-        );
-      },
-      onCreatePlatformView: (PlatformViewCreationParams params) {
-        var platformView = PlatformViewsService.initSurfaceAndroidView(
-          id: params.id,
-          viewType: SHOW_FORM_VIEW_TYPE,
-          layoutDirection: TextDirection.ltr,
-          creationParams: creationParams,
-          creationParamsCodec: StandardMessageCodec(),
-        );
-        platformView
-            .addOnPlatformViewCreatedListener(params.onPlatformViewCreated);
-        platformView
-            .addOnPlatformViewCreatedListener(_createCardShowController);
-        platformView.create();
-        return platformView;
-      },
-    );
-  }
+
 
   Widget _cardCollectNativeiOS() {
     final Map<String, dynamic> creationParams = <String, dynamic>{};
@@ -260,58 +239,147 @@ class _MyHomePageState extends State<MyHomePage> {
     ]);
   }
 
-  Widget _cardShowNativeiOS() {
-    final Map<String, dynamic> creationParams = <String, dynamic>{};
-    return Column(children: [
-      SizedBox(
-          height: 136.0,
-          child: UiKitView(
-              viewType: SHOW_FORM_VIEW_TYPE,
-              onPlatformViewCreated: _createCardShowController,
-              creationParams: creationParams,
-              creationParamsCodec: StandardMessageCodec()))
-    ]);
-  }
 
-  Widget _showButton() {
-    return MaterialButton(
-      minWidth: MediaQuery.of(context).size.width,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-      color: Colors.blue,
-      child: new Text('Reveal',
-          style: new TextStyle(fontSize: 16.0, color: Colors.white)),
-      onPressed: () {
-        showController.revealCardAsync(cardToken,nameToken ,dateToken,cvcToken,ssnToken).then((value) {
-          final entries = value.entries;
-          print("show value.entries: $entries");
+}
+class _ShowHomePageState extends State<ShowHomePage> {
+CardShowFormController showController;
 
-          final errorCode = value[SHOW_ERROR_CODE_KEY];
-          if (errorCode != null) {
-            print("error!");
-            print("VGS Show SDK error code: $errorCode");
+ String cardToken ;
+ String dateToken;
+ String nameToken;
+ String cvcToken;
+ String ssnToken;
 
-            final errorMessage = value[SHOW_ERROR_MESSAGE_KEY];
-            if (errorMessage != null) {
-              print("VGS Show SDK error message: $errorMessage");
-            }
-            return;
-          }
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:
+      SingleChildScrollView(
+        child:
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
 
-          final successStatusCode = value[SHOW_SUCCESS_CODE_KEY];
-          if (successStatusCode != null) {
-            print("VGS Show success status code: $successStatusCode");
-          }
-        });
-      },
-    );
-  }
+              ConstrainedBox(
+                  constraints: BoxConstraints(maxHeight: 400),
+                  child: Column(children: <Widget>[
+                    Text(
+                      'Show Card Details',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey.shade700),
+                    ),
+                    Flexible(child: Builder(builder: (context) {
+                      // any logic needed...
+                      final isAndorid =
+                          defaultTargetPlatform == TargetPlatform.android;
 
-  void _createCardShowController(int id) {
-    print("Show View id = $id");
-    showController = new CardShowFormController._(id);
-  }
+                      return isAndorid ? _cardShow() : _cardShowNativeiOS();
+                    })),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 12.0),
+                      child: _showButton(),
+                    ),
+                  ])),
+            ],
+          ),
+        ),
+
+
+      )    //       children: <Widget>[
+
+  );
 }
 
+
+
+Widget _cardShow() {
+  final Map<String, dynamic> creationParams = <String, dynamic>{};
+  return PlatformViewLink(
+    viewType: SHOW_FORM_VIEW_TYPE,
+    surfaceFactory:
+        (BuildContext context, PlatformViewController controller) {
+      return AndroidViewSurface(
+        controller: controller,
+        gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+        hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+      );
+    },
+    onCreatePlatformView: (PlatformViewCreationParams params) {
+      var platformView = PlatformViewsService.initSurfaceAndroidView(
+        id: params.id,
+        viewType: SHOW_FORM_VIEW_TYPE,
+        layoutDirection: TextDirection.ltr,
+        creationParams: creationParams,
+        creationParamsCodec: StandardMessageCodec(),
+      );
+      platformView
+          .addOnPlatformViewCreatedListener(params.onPlatformViewCreated);
+      platformView
+          .addOnPlatformViewCreatedListener(_createCardShowController);
+      platformView.create();
+      return platformView;
+    },
+  );
+}
+
+
+Widget _cardShowNativeiOS() {
+  final Map<String, dynamic> creationParams = <String, dynamic>{};
+  return Column(children: [
+    SizedBox(
+        height: 136.0,
+        child: UiKitView(
+            viewType: SHOW_FORM_VIEW_TYPE,
+            onPlatformViewCreated: _createCardShowController,
+            creationParams: creationParams,
+            creationParamsCodec: StandardMessageCodec()))
+  ]);
+}
+
+Widget _showButton() {
+  return MaterialButton(
+    minWidth: MediaQuery.of(context).size.width,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+    color: Colors.blue,
+    child: new Text('Reveal',
+        style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+    onPressed: () {
+      showController.revealCardAsync(widget.cardToken,widget.nameToken ,widget.dateToken,widget.cvcToken,widget.ssnToken).then((value) {
+        final entries = value.entries;
+        print("show value.entries: $entries");
+
+        final errorCode = value[SHOW_ERROR_CODE_KEY];
+        if (errorCode != null) {
+          print("error!");
+          print("VGS Show SDK error code: $errorCode");
+
+          final errorMessage = value[SHOW_ERROR_MESSAGE_KEY];
+          if (errorMessage != null) {
+            print("VGS Show SDK error message: $errorMessage");
+          }
+          return;
+        }
+
+        final successStatusCode = value[SHOW_SUCCESS_CODE_KEY];
+        if (successStatusCode != null) {
+          print("VGS Show success status code: $successStatusCode");
+        }
+      });
+    },
+  );
+}
+
+void _createCardShowController(int id) {
+  print("Show View id = $id");
+  showController = new CardShowFormController._(id);
+}
+}
 class CardCollectFormController {
   CardCollectFormController._(int id)
       : _channel = new MethodChannel('$COLLECT_FORM_VIEW_TYPE/$id');
